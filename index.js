4e0cb838e174bb5acfcad9070a0f3b886eccca9e
gr7800 <gt29015@gmail.com>
2023-01-05 13:08:54 +0530

    backend server completed

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const jsonServer = require('json-server')
-const cors =require("cors");
 
 const server = jsonServer.create()
 
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-server.use(cors)
+ 
 server.use(middlewares)
-
 server.use('/', router)
 server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running')
